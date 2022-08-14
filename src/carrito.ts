b193b270ca40417e0cf3d808bae701a91c49d62f
SilvioAlvarez <104571960+SilvioAlvarez@users.noreply.github.com>
2022-08-14 17:58:47 -0300

    Trabajo Final Terminado

--- src/carrito.ts
+++ src/carrito.ts
@@ -7,13 +7,17 @@ let agregar1 = document.getElementById("btn-agregar1")
 let totales = document.getElementById("totales");
 let totales1 = document.getElementById("totales1");
 let totales2 = document.getElementById("totales2");
+let totales3 = document.getElementById("totales3");
 let productos: string[] = [];
 let productos1: string[] = [];
 let precios: number []= [];
 let precios1: number []= [];
 let suma: number = 0;
 let suma1: number = 0;
-let suma2: number = 0;
+let descuento: number = 0;
+let montocondescuento: number = 0;
+
+
 let vaciar = document.getElementById("vaciar");
 //metodo push para arreglos
 
@@ -30,9 +34,18 @@ suma = 0;
     }
     totales?.innerHTML=suma; 
     totales2?.innerHTML=suma + suma1;
-    }
 
+if (totales2.innerHTML >= 150000){
+  descuento = totales2.innerHTML*15/100;
+  montocondescuento = totales2.innerHTML-descuento;
 
+totales3?.innerHTML = montocondescuento;
+}
+else{
+  totales3?.innerHTML="Con su compra de $150000 puede acceder a un descuento";
+}}
+    
+    
 const agregarAlCarrito1 = () => {
     productos1.push(producto1.value1);
     precios1.push(Number(precio1.value));
@@ -40,13 +53,26 @@ const agregarAlCarrito1 = () => {
   lista1?.innerHTML += `<li>${producto1.value} : $${precio1.value}</li>`;
 
  suma1 = 0;
+ descuento=0;
+ montocondescuento=0;
       for (let i: number=0; i < precios1.length; i++){
         suma1 += Number(precios1[i]);      
      }
      totales1?.innerHTML=suma1; 
     
     totales2?.innerHTML=suma + suma1;
+
+if (totales2.innerHTML >= 150000){
+  descuento = totales2.innerHTML*15/100;
+  montocondescuento = totales2.innerHTML-descuento;
+
+totales3?.innerHTML = montocondescuento;
 }
+else{
+  totales3?.innerHTML="Con su compra de $150000 puede acceder a un descuento";
+}}
+
+
 
 const vaciarLista = () => {
  lista?.innerHTML = "";
@@ -54,6 +80,8 @@ const vaciarLista = () => {
  totales?.innerHTML = "";
  totales1?.innerHTML = "";
  totales2?.innerHTML= "";
+ totales3?.innerHTML= "";
+
   }
 agregar?.addEventListener("click", agregarAlCarrito);
 agregar1?.addEventListener("click", agregarAlCarrito1);
